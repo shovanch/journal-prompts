83be11f1289d8c6ffd9b0d7a638e6111d029a523
Shovan Chatterjee <shovanc.web@gmail.com>
2019-08-04 16:04:46 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Hi, I am Shovan - The developer</h1>
+          <h2>Let's Code</h2>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
